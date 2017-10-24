82106b19b4d3e6a21e874bad460f8e966cfed76e
BlueTone <bluetonemaster@gmail.com>
2017-10-25 00:30:17 +0900

    mp3プレーヤー用スケッチ

---
+++ mp3player/mp3player.ino
@@ -0,0 +1,18 @@
+// 入力ピンが入力状態の時、出力ピンを出力する
+
+const int inPin  = 11;  // 入力ピン
+const int outPin = 12;  // 出力ピン
+
+void setup() {
+  pinMode(inPin, INPUT_PULLUP);  
+  pinMode(outPin, OUTPUT);  
+}
+
+void loop() {
+  if(LOW == digitalRead(inPin)){
+    digitalWrite(outPin, HIGH);
+  }else{
+    digitalWrite(outPin, LOW);
+  }
+  delay(100);
+}
